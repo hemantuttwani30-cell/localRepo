cdb0dd880f9b842c62d52bbc6701cdc962db3a26
Hemant_Uttwani <Hemantuttwani30@gmail.com>
2026-05-31 22:53:32 +0530

    Add your

--- Second.c
+++ Second.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("Second commit");
-    printf("Hello World");
+    printf("Hello (your) World");
     printf("My name is Hemant Uttwani");
     return 0;
 }
